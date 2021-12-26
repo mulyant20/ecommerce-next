155f5e33fb4a927784f635b688ada14d3bfbf0ae
mulyant20 <mulyan.t20@gmail.com>
2021-12-27 00:13:10 +0700

    feat: add Layout component

--- components/Layout.js
+++ components/Layout.js
@@ -1,5 +1,22 @@
-export default function Layout() {
+import Head from 'next/head'
+import Footer from './Footer'
+import Navbar from './Navbar'
+
+export default function Layout({children}) {
     return(
-        <div>Layout</div>
+        <div className='layout'>
+            <Head>
+                <title>Simple Ecommerce</title>
+            </Head>
+            <header>
+                <Navbar />
+            </header>
+            <main className='main-container'>
+                {children}
+            </main>
+            <footer>
+                <Footer />
+            </footer>
+        </div>
     )
 }
