8744e4bb36d4e3d804b4de75efa5f6dbecd97616
mulyant20 <mulyan.t20@gmail.com>
2021-12-26 23:51:09 +0700

    build: sanity setup

---
+++ components/FooterBanner.js
@@ -0,0 +1,40 @@
+import Link from "next/link";
+
+import { urlFor } from "../lib/client";
+
+export default function FooterBanner({
+  footerBanner: {
+    discount,
+    largeText1,
+    largeText2,
+    smallText,
+    midText,
+    desc,
+    product,
+    buttonText,
+    image,
+    saleTime
+  },
+}) {
+  return (
+    <div className="footer-banner-container">
+      <div className="banner-desc">
+        <div className="left">
+          <p>{discount}</p>
+          <h3>{largeText1}</h3>
+          <h3>{largeText2}</h3>
+          <p>{saleTime}</p>
+        </div>
+        <div className="right">
+          <p>{smallText}</p>
+          <h3>{midText}</h3>
+          <p>{desc}</p>
+          <Link href={`/product/${product}`}>
+            <button type="button">{buttonText}</button>
+          </Link>
+        </div>
+        <img src={urlFor(image)} className="footer-banner-image" />
+      </div>
+    </div>
+  );
+}
---
+++ pages/index.js
@@ -0,0 +1,34 @@
+import { Product, FooterBanner, HeroBanner } from "../components";
+import { client } from "../lib/client";
+
+export default function Home({ products, bannerData }) {
+  console.log(bannerData);
+  return (
+    <>
+      <HeroBanner heroBanner={bannerData.length && bannerData[0]} />
+
+      <div className="products-heading">
+        <h2>Best Selling Products</h2>
+        <p>Speakers of many variotions</p>
+      </div>
+      <div className="products-container">
+        {products.map((product) => (
+          <Product key={product._id} product={product} />
+        ))}
+      </div>
+      <FooterBanner footerBanner={bannerData.length && bannerData[0]} />
+    </>
+  );
+}
+
+export const getServerSideProps = async () => {
+  const productQuery = '*[_type == "product"]';
+  const products = await client.fetch(productQuery);
+
+  const bannerQuery = '*[_type == "banner"]';
+  const bannerData = await client.fetch(bannerQuery);
+
+  return {
+    props: { products, bannerData },
+  };
+};
